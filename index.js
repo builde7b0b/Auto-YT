0177b26576d1db79823726c9b1a5c228c8cd45ec
builde7b0b <ggives3@gmail.com>
2023-05-30 13:25:21 -0700

    implement function

--- index.js
+++ index.js
@@ -3,4 +3,12 @@ const { google } = require('googleapis');
 const { auth } = require('google-auth-library');
 
 
-async function authenticate() {}
+async function authenticate() {
+
+    const client = await auth.getClient({
+        keyFile: 'path/to/client_secret.json',
+        scopes: 'https://www.googleapis.com/auth/youtube.upload',
+    });
+
+    google.options({ auth: client })
+}
